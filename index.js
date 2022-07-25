cf2d3826070ad54eab07879e2172b81e2c1a46b0
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-07-25 16:01:47 -0500

    Adding places view with mock data

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const PORT=process.env.PORT
 app.get('/',(req,res)=>{
     res.render('home')
 })
-app.use('/places', require('./controllers/places'))
+//app.use('/places', require('./controllers/places'))
+app.get('/',(req,res)=>{
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index',{places})
+})
 
 
 app.get('*',(req,res)=>{
