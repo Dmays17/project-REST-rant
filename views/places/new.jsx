e9795f6d55b65cce4b2aaa220b831fad9b6faec4
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-07-27 20:02:12 -0500

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,30 @@ return (
 <Def>
     <main>
         <h1>Add a New Place</h1>
+        <form method="POST" action="/places">
+  <div className='form-control'>
+    <label htmlFor="name">Place Name</label>
+    <input className='form-control' id="name" name="name" />
+  </div>
+  <div className='form-control'>
+    <label htmlFor="pic">Place Picture</label>
+    <input className='form-control' type="url" id="pic" name="pic" />
+  </div>
+  <div className='form-control'>
+    <label htmlFor="city">City</label>
+    <input className='form-control' id="city" name="city" />
+  </div>
+  <div className='form-control'>
+    <label htmlFor="state">State</label>
+    <input className='form-control' id="state" name="state" />
+  </div>
+  <div className='form-control'>
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className='form-control' id="cuisines" name="cuisines" />
+  </div>
+  <input className='btn btn-primary' type="submit" value="Add Place" />
+</form>
+
     </main>
 </Def>
 )
