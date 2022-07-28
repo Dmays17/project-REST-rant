3168c55f32c253f5d595e29d9151ca3dc6bf335a
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-07-27 20:20:51 -0500

    Form data is getting through!

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ app.set('view engine','jsx')
 app.engine('jsx',require('express-react-views').createEngine())
 app.use(express.static('public'))
 const PORT=process.env.PORT
+app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     res.render('home')
 })
-//app.use('/places', require('./controllers/places'))
+
 app.use('/places',require('./controllers/places'))
 
 
 app.get('*',(req,res)=>{
-    res.render('error404')
+    res.render('error404') 
 })
 
 app.listen(PORT)
