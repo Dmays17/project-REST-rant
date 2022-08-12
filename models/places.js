bdf2a5638ac78454718c71b13c69ad8074bf7b18
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-08-12 08:47:00 -0500

    catch year error

--- models/places.js
+++ models/places.js
@@ -5,7 +5,7 @@ pic:{ type: String, default: 'http://placekitten.com/350/350'},
 cuisines:{type:String, required:true},
 city:{type:String, defaule:'Anytown'},
 state:{type:String, defaule:'USA'},
-founder:{
+founded:{
     type:Number,
     min:[1673,'Surely not that old?'],
     max:[new Date().getFullYear(),'Hey, this year is in the future!']
@@ -18,19 +18,3 @@ placeSchema.methods.showEstablished=function(){
 }
 module.exports=mongoose.model('Place',placeSchema)
 
-
-
-
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/pancakes.jpeg'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/wings.jpeg'
-// }]
