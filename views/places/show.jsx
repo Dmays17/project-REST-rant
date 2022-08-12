1723b0ca33d35397755bc531187a0edd1e2bd3a9
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-08-12 09:40:16 -0500

    adding comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,26 @@ const React= require('react')
 const Def= require('../default')
 
 function show(data){
+    let comments=(
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+        if (data.place.comments.length){
+            comments = data.place.comments.map(c =>{
+                return(
+                    <div className='border'>
+                        <h2 className= "rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                        <h4>{c.content}</h4>
+                        <h3>
+                            <strong>- {c.author}</strong>
+                        </h3>
+                        <h4>Rating:{c.stars}</h4>
+                    </div>
+                )
+            })
+        }
+    
     return(
         <Def>        
             <main>
@@ -13,6 +33,8 @@ function show(data){
                 <h2>Description </h2>
                 <h4>{data.place.showEstablished()}</h4>
                 <h3>Serving {data.place.cuisines}</h3>
+                <h2>Comments</h2>
+                {comments}
             </main>
             <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
             
@@ -24,6 +46,6 @@ function show(data){
         </Def>
 
     )
-}
+    }
 
 module.exports=show
