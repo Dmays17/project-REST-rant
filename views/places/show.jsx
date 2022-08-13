782c535ba4d8df62e5b3a7105f655e3c44ea8619
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-08-13 08:34:26 -0500

    add stars to show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,28 @@ function show(data){
             No comments yet!
         </h3>
     )
+        let rating =(
+            <h3 className='inactive'>
+                Not yet rated
+            </h3>
+        )
+
         if (data.place.comments.length){
+            let sumRatings=data.place.comments.reduce((tot,c)=>{
+                return tot + c.stars
+            },0)
+            let averageRating = sumRatings / data.place.comments.length
+            rating=(
+                <h3>
+                   {Math.round(averageRating)} stars
+                </h3>
+            )
+
+
+
+
+
+
             comments = data.place.comments.map(c =>{
                 return(
                     <div className='border'>
@@ -29,7 +50,8 @@ function show(data){
                 <img src={data.place.pic}></img>
                 <h1>{data.place.name}</h1>
                 <h2>Rating</h2>
-                <h4>Not rated</h4>
+                {rating}
+                
                 <h2>Description </h2>
                 <h4>{data.place.showEstablished()}</h4>
                 <h3>Serving {data.place.cuisines}</h3>
